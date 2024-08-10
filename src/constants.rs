b74952fd4fe320622adb907229494a05ed47bb03
root <neithercouldterry@gmail.com>
2024-08-10 17:52:12 -0600

    changed the file table format

--- src/constants.rs
+++ src/constants.rs
@@ -1,8 +1,8 @@
 
-
+/// The cryptographic key length in bytes.
 pub const KEY_LENGTH_IN_BYTES: usize = 32;
 
-// Size of chunks to write files in. (128 MiB)
+/// Size of chunks to write files in. (128 MiB)
 pub const CHUNK_SIZE: usize = 131_072;
 
 /// The length of a salt in bytes.
--- src/structure/table.rs
+++ src/structure/table.rs
@@ -1,5 +1,5 @@
-use std::io::{Cursor, Read, Seek, SeekFrom};
-use crate::{ioutils::{read_bool, read_pathbuf, read_u32, read_u64}, security::secure::{create_key, read_encrypted}};
+use std::io::{Cursor, Read, Seek, SeekFrom, Write};
+use crate::{constants::SALT_LENGTH_IN_BYTES, ioutils::{read_bool, read_pathbuf, read_u32, read_u64, write_bool, write_pathbuf}, security::secure::{create_key, read_encrypted, write_encrypted}};
 use anyhow::Result;
 use super::node::ArchivalNode;
 
@@ -13,50 +13,105 @@ pub struct FileTable {
     /// (POS, POS, NODE)
     pub map: Vec<(u32, u64, ArchivalNode)>,
     /// The encryption key being used for the table.
-    pub key: Vec<u8>
+    key: Vec<u8>,
+    salt: Vec<u8>
+    
 }
 
 impl FileTable {
     /// Creates a blank new file table.
-    pub fn new(key: &[u8]) -> Self {
+    pub fn new(key: &[u8], salt: &[u8]) -> Self {
         Self {
             map: Vec::default(),
-            key: key.to_vec()
+            key: key.to_vec(),
+            salt: salt.to_vec()
+          //  salt
         }
+    }
+    /// Adds a node to the file table structure.
+    ///
+    /// Takes:
+    /// - Index (position within the file table)
+    /// - File Index (position within the file)
+    /// - Node, an [ArchivalNode] representing the object to represent
+    /// within the table.
+    pub fn add(&mut self, index: u32, file_index: u64, node: ArchivalNode) {
+        self.map.push((index, file_index, node))
     }
     /// Creates a `FileTable` from a mutable reader object.
     pub fn from_reader<T: Read + Seek>(reader: &mut T, password: &str) -> Result<Self> {
         read_file_table(reader, password)
+    }
+    /// Writes the file table to a [Writer](std::io) object.
+    pub fn write<T: Write + Seek>(&self, writer: &mut T) -> Result<()> {
+        write_file_table(writer, self)
     }
 //    pub fn expand_into_files<T: Read + Seek>(&self, reader: &mut T, dest: impl AsRef<Path>) -> Result<()> {
  //       
    // }
 }
 
+fn write_file_table<T: Write + Seek>(writer: &mut T, table: &FileTable) -> Result<()> {
+    let current_position = writer.stream_position()?;
+
+
+
+    writer.write_all(&table.salt)?;
+
+
+    // Create a write to to write pre-encryption.
+    let mut table_writer = Cursor::new(Vec::new());
+
+    for (key, value, node) in table.map.iter() {
+        table_writer.write_all(&key.to_le_bytes())?;
+        table_writer.write_all(&value.to_le_bytes())?;
+
+        write_bool(&mut table_writer, node.is_leaf)?;
+        write_pathbuf(&mut table_writer, &node.path)?;
+    }
+
+    write_encrypted(writer, &table.key, table_writer.into_inner().as_ref())?;
+
+    writer.write_all(&current_position.to_le_bytes())?;
+   
+
+//    println!("Done writing {:?}", writer.stream_position()?);
+    Ok(())
+}
+
+
+
 fn read_file_table<T: Read + Seek>(reader: &mut T, password: &str) -> Result<FileTable> {
 
-    reader.seek(SeekFrom::Start(0))?;
+   // reader.seek(SeekFrom::Start(0))?;
     
-    let salt = &mut [0u8; 32];
-    reader.read_exact(salt)?;
+//    let mut salt = vec![0u8; SALT_LENGTH_IN_BYTES];
+   // let salt = &mut [0u8; SALT_LENGTH_IN_BYTES];
+   // reader.read_exact(salt)?;
 
-    let key = &create_key(salt, password.as_bytes())?;
 
     reader.seek(SeekFrom::End(-8))?;
-//    let stop_position = reader.stream_position()?;
+
+    //    let stop_position = reader.stream_position()?;
+
+
 
     let table_position = read_u64(reader)?;
     //println!("Table position: {table_position}");
     reader.seek(SeekFrom::Start(table_position))?;
 
-    
+    let mut salt = vec![0u8; SALT_LENGTH_IN_BYTES];
+    reader.read_exact(&mut salt)?;
+
+    let key = &create_key(&salt, password.as_bytes())?;
+
     // Decrypt the file table.
     let decrypted = read_encrypted(reader, key)?;
 
 
     let decrypted_len = decrypted.len();
     let mut reader = Cursor::new(Vec::from(decrypted));
-    let mut file_table = FileTable::new(key);
+    let mut file_table = FileTable::new(key, &*salt);
 
     loop {
         let key = read_u32(&mut reader)?;
@@ -81,13 +136,35 @@ fn read_file_table<T: Read + Seek>(reader: &mut T, password: &str) -> Result<Fil
 
 #[cfg(test)]
 mod tests {
+    use std::{io::Cursor, path::Path};
+
     use anyhow::Result;
+
+    use crate::security::secure::{create_key, generate_salt};
+
+    use super::FileTable;
 
 
 
     #[test]
     pub fn test_file_table() -> Result<()> {
-        
+        let mut export = Cursor::new(Vec::new());
+
+        let password = "default_password";
+
+
+        let salt = generate_salt();
+        let key = create_key(&salt, password.as_bytes())?;
+
+        let mut file_table = FileTable::new(&key, &salt);
+        file_table.add(0, 32, crate::structure::node::ArchivalNode { path: Path::new("hello").to_path_buf(), is_leaf: true });
+
+        file_table.write(&mut export)?;
+
+
+
+        let mut export = Cursor::new(export.into_inner());
+        let mut file_table = FileTable::from_reader(&mut export, password)?;
 
         Ok(())
     }
